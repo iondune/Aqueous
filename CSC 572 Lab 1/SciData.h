f6ec041dc53163963205514d684a2624c052b8de
iondune <none@none>
2012-06-18 02:35:58 -0700

    + Some basic setup for an oct tree
    --HG--
    branch : VolumeRendering

--- CSC 572 Lab 1/SciData.h
+++ CSC 572 Lab 1/SciData.h
@@ -67,4 +67,54 @@ protected:
 	// tm timestamp;
 };
 
+class ISciTreeNode
+{
+
+public:
+
+	virtual bool const isLeaf() const =0;
+
+};
+
+enum EOctant
+{
+	EO_TOP_UPPER_LEFT,
+	EO_TOP_UPPER_RIGHT,
+	EO_TOP_LOWER_LEFT,
+	EO_TOP_LOWER_RIGHT,
+	EO_BOTTOM_UPPER_LEFT,
+	EO_BOTTOM_UPPER_RIGHT,
+	EO_BOTTOM_LOWER_LEFT,
+	EO_BOTTOM_LOWER_RIGHT,
+	EO_COUNT
+};
+
+class CSciTreeNode : public ISciTreeNode
+{
+
+public:
+
+	ISciTreeNode * Children[EO_COUNT];
+
+	virtual bool const isLeaf() const
+	{
+		return false;
+	}
+
+};
+
+class CSciTreeLeaf : public ISciTreeNode
+{
+
+public:
+
+	std::vector<SciData> Datums;
+
+	virtual bool const isLeaf() const
+	{
+		return true;
+	}
+
+};
+
 #endif
