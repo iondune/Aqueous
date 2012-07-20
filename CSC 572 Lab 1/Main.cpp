c3ca589b352dd0beac404bcd3ec70873a4ab8428
Ian <none@none>
2012-07-19 23:02:22 -0700

    + Updated library loading
    --HG--
    branch : VolumeRendering

--- CSC 572 Lab 1/Main.cpp
+++ CSC 572 Lab 1/Main.cpp
@@ -1,29 +1,22 @@
 #ifdef _WIN32
+
+// OpenGL Libraries
 #pragma comment(lib, "glew32.lib")
 #pragma comment(lib, "OpenGL32.lib")
 #pragma comment(lib, "glu32.lib")
-#pragma comment(lib, "freetype.lib")
 
-#pragma comment(lib, "matlib/libmat.lib")
-#pragma comment(lib, "matlib/libmx.lib")
-#pragma comment(lib, "matlib/libmex.lib")
-#pragma comment(lib, "matlib/libeng.lib")
-#pragma comment(lib, "matlib/libmwlapack.lib")
-#pragma comment(lib, "matlib/libdflapack.lib")
-
-#pragma comment(lib, "ionWindow.lib")
-#pragma comment(lib, "ionScene.lib")
-#pragma comment(lib, "ionGUI.lib")
-
-#pragma comment(lib, "FreeImage.lib")
+// ionEngine Libraries
 #ifdef _DEBUG
-#pragma comment(lib, "GWENd-Renderer-SFML.lib")
-#pragma comment(lib, "gwend_static.lib")
+#pragma comment(lib, "Debug/ionWindow.lib")
+#pragma comment(lib, "Debug/ionScene.lib")
+#pragma comment(lib, "Debug/ionGUI.lib")
 #else
-#pragma comment(lib, "GWEN-Renderer-SFML.lib")
-#pragma comment(lib, "gwen_static.lib")
+#pragma comment(lib, "Release/ionWindow.lib")
+#pragma comment(lib, "Release/ionScene.lib")
+#pragma comment(lib, "Release/ionGUI.lib")
 #endif
 
+// SFML Libraries
 #ifdef _DEBUG
 #pragma comment(lib, "sfml-system-s-d.lib")
 #pragma comment(lib, "sfml-window-s-d.lib")
@@ -33,6 +26,24 @@
 #pragma comment(lib, "sfml-window-s.lib")
 #pragma comment(lib, "sfml-graphics-s.lib")
 #endif
+
+// GWEN Libraries
+#pragma comment(lib, "FreeImage.lib")
+#ifdef _DEBUG
+#pragma comment(lib, "GWENd-Renderer-SFML.lib")
+#pragma comment(lib, "gwend_static.lib")
+#else
+#pragma comment(lib, "GWEN-Renderer-SFML.lib")
+#pragma comment(lib, "gwen_static.lib")
+#endif
+
+// MatLAB Libraries
+#pragma comment(lib, "matlib/libmat.lib")
+#pragma comment(lib, "matlib/libmx.lib")
+#pragma comment(lib, "matlib/libmex.lib")
+#pragma comment(lib, "matlib/libeng.lib")
+#pragma comment(lib, "matlib/libmwlapack.lib")
+#pragma comment(lib, "matlib/libdflapack.lib")
 
 #endif
 
