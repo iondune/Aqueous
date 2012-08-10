4c7a65bbb69b4d6ff6cb180b72ff432306e8d42a
iondune <none@none>
2012-08-09 21:09:54 -0700

    + Added volume controls to window

--- VolumeVisualization/CGUIVolumeControlWidget.cpp
+++ VolumeVisualization/CGUIVolumeControlWidget.cpp
@@ -6,51 +6,57 @@
 #include <Gwen/Controls.h>
 #include <Gwen/Controls/HorizontalSlider.h>
 #include <Gwen/Controls/ComboBox.h>
+#include <Gwen/Controls/WindowControl.h>
 
 
 CGUIVolumeControlWidget::CGUIVolumeControlWidget()
 	: VolumeControl(CProgramContext::get().Scene.VolumeSceneObject->Control), MainState(CMainState::get())
 {
+	Gwen::Controls::WindowControl * Window = new Gwen::Controls::WindowControl(GUIManager->getCanvas());
+	Window->SetDeleteOnClose(false);
+	Window->SetBounds(1200, 0, 330, 450);
+	Window->SetTitle("Volume Controls");
+
 	// Slider Panel
 	{
-		Gwen::Controls::Label * SliderLabel = new Gwen::Controls::Label(GUIManager->getCanvas());
+		Gwen::Controls::Label * SliderLabel = new Gwen::Controls::Label(Window);
 		SliderLabel->SetFont(GUIManager->getRegularFont());
 		SliderLabel->SetText(L"Selection Value:");
-		SliderLabel->SetBounds(1250, 10, 300, 40);
-		SliderLabel->SetTextColor(Gwen::Color(255, 200, 200, 215));
+		SliderLabel->SetBounds(10, 10, 300, 40);
+		SliderLabel->SetTextColor(Gwen::Color(50, 20, 20, 215));
 
-		Gwen::Controls::HorizontalSlider * EmphasisSlider = new Gwen::Controls::HorizontalSlider(GUIManager->getCanvas());
-		EmphasisSlider->SetBounds(1250, 30, 300, 40);
+		Gwen::Controls::HorizontalSlider * EmphasisSlider = new Gwen::Controls::HorizontalSlider(Window);
+		EmphasisSlider->SetBounds(10, 30, 300, 40);
 		EmphasisSlider->SetRange(0.f, 1.f);
 
-		SliderLabel = new Gwen::Controls::Label(GUIManager->getCanvas());
+		SliderLabel = new Gwen::Controls::Label(Window);
 		SliderLabel->SetFont(GUIManager->getRegularFont());
 		SliderLabel->SetText(L"Volume Opacity:");
-		SliderLabel->SetBounds(1250, 70, 300, 40);
-		SliderLabel->SetTextColor(Gwen::Color(255, 200, 200, 215));
+		SliderLabel->SetBounds(10, 70, 300, 40);
+		SliderLabel->SetTextColor(Gwen::Color(50, 20, 20, 215));
 
-		Gwen::Controls::HorizontalSlider * IntensitySlider = new Gwen::Controls::HorizontalSlider(GUIManager->getCanvas());
-		IntensitySlider->SetBounds(1250, 90, 300, 40);
+		Gwen::Controls::HorizontalSlider * IntensitySlider = new Gwen::Controls::HorizontalSlider(Window);
+		IntensitySlider->SetBounds(10, 90, 300, 40);
 		IntensitySlider->SetRange(10.f, 0.5f);
 
-		SliderLabel = new Gwen::Controls::Label(GUIManager->getCanvas());
+		SliderLabel = new Gwen::Controls::Label(Window);
 		SliderLabel->SetFont(GUIManager->getRegularFont());
 		SliderLabel->SetText(L"Selection Range:");
-		SliderLabel->SetBounds(1250, 130, 300, 40);
-		SliderLabel->SetTextColor(Gwen::Color(255, 200, 200, 215));
+		SliderLabel->SetBounds(10, 130, 300, 40);
+		SliderLabel->SetTextColor(Gwen::Color(50, 20, 20, 215));
 
-		Gwen::Controls::HorizontalSlider * LocalRangeSlider = new Gwen::Controls::HorizontalSlider(GUIManager->getCanvas());
-		LocalRangeSlider->SetBounds(1250, 150, 300, 40);
+		Gwen::Controls::HorizontalSlider * LocalRangeSlider = new Gwen::Controls::HorizontalSlider(Window);
+		LocalRangeSlider->SetBounds(10, 150, 300, 40);
 		LocalRangeSlider->SetRange(0.05f, 0.5f);
 
-		SliderLabel = new Gwen::Controls::Label(GUIManager->getCanvas());
+		SliderLabel = new Gwen::Controls::Label(Window);
 		SliderLabel->SetFont(GUIManager->getRegularFont());
 		SliderLabel->SetText(L"Selection Difference:");
-		SliderLabel->SetBounds(1250, 190, 300, 40);
-		SliderLabel->SetTextColor(Gwen::Color(255, 200, 200, 215));
+		SliderLabel->SetBounds(10, 190, 300, 40);
+		SliderLabel->SetTextColor(Gwen::Color(50, 20, 20, 215));
 
-		Gwen::Controls::HorizontalSlider * MinimumAlphaSlider = new Gwen::Controls::HorizontalSlider(GUIManager->getCanvas());
-		MinimumAlphaSlider->SetBounds(1250, 210, 300, 40);
+		Gwen::Controls::HorizontalSlider * MinimumAlphaSlider = new Gwen::Controls::HorizontalSlider(Window);
+		MinimumAlphaSlider->SetBounds(10, 210, 300, 40);
 		MinimumAlphaSlider->SetRange(0.0f, 0.5f);
 
 		// Wire Up Events
@@ -63,38 +69,38 @@ CGUIVolumeControlWidget::CGUIVolumeControlWidget()
 	
 	// Other Controls Panel
 	{
-		Gwen::Controls::Label * ControlLabel = new Gwen::Controls::Label(GUIManager->getCanvas());
+		Gwen::Controls::Label * ControlLabel = new Gwen::Controls::Label(Window);
 		ControlLabel->SetFont(GUIManager->getRegularFont());
 		ControlLabel->SetText(L"Mode:");
-		ControlLabel->SetBounds(1300, 320, 90, 40);
-		ControlLabel->SetTextColor(Gwen::Color(255, 255, 200, 215));
+		ControlLabel->SetBounds(30, 320, 90, 40);
+		ControlLabel->SetTextColor(Gwen::Color(50, 50, 20, 215));
 
-		Gwen::Controls::ComboBox * VolumeMode = new Gwen::Controls::ComboBox(GUIManager->getCanvas());
-		VolumeMode->SetBounds(1350, 120 + 120 + 45 + 35, 200, 25);
+		Gwen::Controls::ComboBox * VolumeMode = new Gwen::Controls::ComboBox(Window);
+		VolumeMode->SetBounds(80, 120 + 120 + 45 + 35, 200, 25);
 		VolumeMode->AddItem(L"Full Volume");
 		VolumeMode->AddItem(L"Plane Slices");
 		VolumeMode->AddItem(L"Isosurface");
 
-		Gwen::Controls::Button * pButton2 = new Gwen::Controls::Button(GUIManager->getCanvas());
-		pButton2->SetBounds(1350, 120 + 120 + 10 + 35, 200, 25);
+		Gwen::Controls::Button * pButton2 = new Gwen::Controls::Button(Window);
+		pButton2->SetBounds(80, 120 + 120 + 10 + 35, 200, 25);
 		pButton2->SetText("Reset Alpha Intensity");
 	
-		ControlLabel = new Gwen::Controls::Label(GUIManager->getCanvas());
+		ControlLabel = new Gwen::Controls::Label(Window);
 		ControlLabel->SetFont(GUIManager->getRegularFont());
 		ControlLabel->SetText(L"Axis:");
-		ControlLabel->SetBounds(1313, 355, 90, 40);
-		ControlLabel->SetTextColor(Gwen::Color(255, 255, 200, 215));
+		ControlLabel->SetBounds(43, 355, 90, 40);
+		ControlLabel->SetTextColor(Gwen::Color(50, 50, 20, 215));
 
-		Gwen::Controls::Button * pButtonX = new Gwen::Controls::Button(GUIManager->getCanvas());
-		pButtonX->SetBounds(1350, 120 + 120 + 10 + 45 + 25 + 35, 40, 25);
+		Gwen::Controls::Button * pButtonX = new Gwen::Controls::Button(Window);
+		pButtonX->SetBounds(80, 120 + 120 + 10 + 45 + 25 + 35, 40, 25);
 		pButtonX->SetText("X");
 
-		Gwen::Controls::Button * pButtonY = new Gwen::Controls::Button(GUIManager->getCanvas());
-		pButtonY->SetBounds(1400, 120 + 120 + 10 + 45 + 25 + 35, 40, 25);
+		Gwen::Controls::Button * pButtonY = new Gwen::Controls::Button(Window);
+		pButtonY->SetBounds(130, 120 + 120 + 10 + 45 + 25 + 35, 40, 25);
 		pButtonY->SetText("Y");
 
-		Gwen::Controls::Button * pButtonZ = new Gwen::Controls::Button(GUIManager->getCanvas());
-		pButtonZ->SetBounds(1450, 120 + 120 + 10 + 45 + 25 + 35, 40, 25);
+		Gwen::Controls::Button * pButtonZ = new Gwen::Controls::Button(Window);
+		pButtonZ->SetBounds(180, 120 + 120 + 10 + 45 + 25 + 35, 40, 25);
 		pButtonZ->SetText("Z");
 
 		// Wire Up Events
