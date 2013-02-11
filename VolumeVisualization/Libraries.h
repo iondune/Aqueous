6bc61c9c2dd0440a96a39c50ab5e287444019baf
Ian <none@none>
2013-02-11 03:59:05 -0800

    + Updated libraries

--- VolumeVisualization/Libraries.h
+++ VolumeVisualization/Libraries.h
@@ -14,13 +14,13 @@
 
 // SFML Libraries
 #ifdef _DEBUG
-#pragma comment(lib, "sfml-system-d.lib")
-#pragma comment(lib, "sfml-window-d.lib")
-#pragma comment(lib, "sfml-graphics-d.lib")
+#pragma comment(lib, "sfml-system-s-d.lib")
+#pragma comment(lib, "sfml-window-s-d.lib")
+#pragma comment(lib, "sfml-graphics-s-d.lib")
 #else
-#pragma comment(lib, "sfml-system.lib")
-#pragma comment(lib, "sfml-window.lib")
-#pragma comment(lib, "sfml-graphics.lib")
+#pragma comment(lib, "sfml-system-s.lib")
+#pragma comment(lib, "sfml-window-s.lib")
+#pragma comment(lib, "sfml-graphics-s.lib")
 #endif
 
 // GWEN Libraries
