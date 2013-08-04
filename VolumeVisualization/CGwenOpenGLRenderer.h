54d54ab096c05e5f1cb3b7a6b02b7f209fcf2e50
Ian <none@none>
2013-08-03 17:00:34 -0700

    + Work on OpenGL font renderer interface cleanup

--- VolumeVisualization/CGwenOpenGLRenderer.h
+++ VolumeVisualization/CGwenOpenGLRenderer.h
@@ -13,16 +13,11 @@ public:
 
 	void Init();
 
-	void RenderText( Gwen::Font* pFont, Gwen::Point pos, const Gwen::UnicodeString & text );
-	Gwen::Point MeasureText( Gwen::Font* pFont, const Gwen::UnicodeString & text );
+	void RenderText(Gwen::Font* pFont, Gwen::Point pos, const Gwen::UnicodeString & text);
+	Gwen::Point MeasureText(Gwen::Font* pFont, const Gwen::UnicodeString & text);
 
 protected:
 
-	void CreateDebugFont();
-	void DestroyDebugFont();
-
-	Gwen::Texture*	m_pFontTexture;
-	float			m_fFontScale[2];
-	float			m_fLetterSpacing;
+	void LoadFont();
 
 };
