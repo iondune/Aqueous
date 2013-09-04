433bc43d7bc5145bd54bbd8007c62ec142ff83c8
Ian <none@none>
2013-09-04 03:45:50 -0700

    + Simplified hopavagen data set creation

--- VolumeVisualization/CMainMenuState.cpp
+++ VolumeVisualization/CMainMenuState.cpp
@@ -41,9 +41,9 @@ void CMainMenuState::Update(f32 const Elapsed)
 	
 	if (! counter--)
 	{
-		//createDataSet();
+		createDataSet();
 		loadData(//"DenmarkMission1.dat");
-			"HopavagenBay1.dat");
+			"HopavagenBayNew1.dat");
 	}
 
 	//loaded = true;
@@ -71,69 +71,15 @@ void CMainMenuState::loadData(std::string const & FileName)
 
 void CMainMenuState::createDataSet()
 {
-	if (false)
-	{
-		SciDataParserSimpleTXT * Parser = new SciDataParserSimpleTXT();
-		Parser->Manager = Context->DataManager;
-		Parser->load("ForZoe.txt");
+	SciDataParserSimpleTXT * Parser1 = new SciDataParserSimpleTXT();
+	Parser1->Manager = Context->DataManager;
+	Parser1->load("ForZoe.txt");
 
-		//DataParser->RawValues.setDataScale(Vector3(3, 2, 3));
-		
-		CSpectrumColorMapper sf("d1");
-		COxygenColorMapper o("d1");
-		sf.AcceptedRange = Range(-9999999.0, 9999999.0);
+	SciDataParserGrid1 * Parser2 = new SciDataParserGrid1();
+	Parser2->Manager = Context->DataManager;
+	Parser2->load("oxyMaps.mat");
 
-		//Context->DataManager->createPointCloudObjects(true, Context->Scene.PointCloudObject, SVector3f(-3.f, 0.8f, 3.f), & o);
-		//DataParser[0]->createGridDataFromRawValues(FullRange, 5.0, "d1");
-		//DataParser[0]->createPointCloudObjects(false, SoupObject, SVector3f(3.f), & sf);
-		//DataParser[0]->createVolumeFromGridValues(& sf);
-	}
-
-	if (false)
-	{
-		SciDataParserCTD * Parser = new SciDataParserCTD();
-		Parser->Manager = Context->DataManager;
-		Parser->examine("oxygenDataSet1.mat");
-
-		//DataParser->RawValues.setDataScale(Vector3(3, 2, 3));
-
-		CSpectrumColorMapper sf("salinity");
-		sf.AcceptedRange = Range(-99999.0, 99999.0);
-
-		//DataParser[1]->createPointCloudObjects(true, VoxelObject, SVector3f(3.f), & sf);
-		Parser->Manager->createGridDataFromRawValues(sf.AcceptedRange, 5.0, "salinity");
-		//DataParser->createPointCloudObjects(false, SoupObject, SVector3f(3.f), & sf);
-		//DataParser->createVolumeFromGridValues(& sf);
-	}
-
-	if (true)
-	{
-		SciDataParserGrid1 * Parser = new SciDataParserGrid1();
-		Parser->Manager = Context->DataManager;
-		Parser->load("oxyMaps.mat");
-
-		//DataParser->GridValues.setDataScale(Vector3(3, 2, 3));
-		
-		CRGBIntensityColorMapper r("o1", "o2", "o3");
-		CSingleFieldColorMapper sf("o1");
-		COxygenColorMapper o;
-		//COxygenLocalizedColorMapper l;
-			
-		Context->DataManager->createVolumeFromGridValues(& o);
-		/*Context->DataManager->createPointCloudObjects(false, Context->Scene.GridObject, SVector3f(3.f), & o);
-		Context->Scene.VolumeSceneObject->VolumeHandle = Context->DataManager->VolumeHandle;*/
-	}
-
-	if (true)
-	{
-		SciDataParserCSV * Parser1 = new SciDataParserCSV();
-		Parser1->Manager = Context->DataManager;
-		Parser1->mergedLoad("OxyData.csv", "LogData.csv", "Time");
-		/*
-		COxygenColorMapper o("AirSaturation(%)");
-
-		Context->DataManager->createPointCloudObjects(true, Context->Scene.PointCloudObject, SVector3f(1.f/*-3.f, 0.8f, 3.f*), & o);*/
-	}
-
-	Context->DataManager->writeToFile("Datasets/HopavagenBayNew1.dat");
+	COxygenColorMapper o;
+	Context->DataManager->createVolumeFromGridValues(& o);
+	Context->DataManager->writeToFile("Datasets/HopavagenBay1.dat");
 }
