5147031cbb29afd8012d8474cc471290a1bd46ee
Ian <none@none>
2013-09-03 20:16:28 -0700

    + Some cleanup, work on distance pass

--- VolumeVisualization/BathymetyGenerator.h
+++ VolumeVisualization/BathymetyGenerator.h
@@ -66,7 +66,7 @@ public:
 		
 		// Blur Pass
 		printf("Guassian blur... ");
-		ProgressPrinter P;
+		ProgressPrinter P(50);
 		P.Begin();
 		static s32 const Sigma = 6;
 		static s32 const Spread = 3;
@@ -139,7 +139,7 @@ public:
 		for (u32 y = 0; y < Height; ++ y)
 		for (u32 x = 0; x < Width; ++ x)
 			MaxMagnitude = Maximum(MaxMagnitude, GetPoint(x, y).Gradient.Length());
-		printf("Maximum pre normal mag is %f\n", MaxMagnitude);
+		//printf("Maximum pre normal mag is %f\n", MaxMagnitude);
 		for (u32 y = 0; y < Height; ++ y)
 		for (u32 x = 0; x < Width; ++ x)
 		{
@@ -147,15 +147,15 @@ public:
 		}
 		Image->Write("OutputPreNormalsMagnitude.bmp");
 
-		FILE * f = fopen("Output.txt", "w");
+		//FILE * f = fopen("Output.txt", "w");
 
 		// Gradient bleed
 		printf("Bleeding gradients... ");
 		P.Begin();
 		s32 CalculatedCount = 0;
 		static s32 const SquareSize = 5;
-		static s32 const Passes = 100;
-		for (s32 t = 0; t < Passes; ++ t)
+		static s32 const Passes = 500;
+		for (s32 t = 0; t < Passes && CalculatedCount < SetCount; ++ t)
 		{
 			for (s32 y = 0; y < (s32) Height; ++ y)
 			for (s32 x = 0; x < (s32) Width; ++ x)
@@ -186,8 +186,8 @@ public:
 					{
 						f32 OriginalLength = GetPoint(x, y).Gradient.Length();
 						GetPoint(x, y).Gradient /= Accumulator;
-						fprintf(f, "%04d %f : [%d] (%d, %f) %f\n", 
-							(s32) GetPoint(x, y).Gradient.Length(), GetPoint(x, y).Gradient.Length(), t, Contributors, Accumulator, OriginalLength);
+						//fprintf(f, "%04d %f : [%d] (%d, %f) %f\n", 
+						//	(s32) GetPoint(x, y).Gradient.Length(), GetPoint(x, y).Gradient.Length(), t, Contributors, Accumulator, OriginalLength);
 						GetPoint(x, y).NewGradientSet = true;
 						CalculatedCount ++;
 					}
@@ -202,68 +202,67 @@ public:
 				if (GetPoint(x, y).NewGradientSet)
 					GetPoint(x, y).GradientSet = true;
 			}
-
-			std::stringstream s1;
-			s1 << "OutputPostNormals" << t << ".bmp";
-			// Diagnostic
-			for (u32 y = 0; y < Height; ++ y)
-			for (u32 x = 0; x < Width; ++ x)
-			{
-				vec2f GradNormal = GetPoint(x, y).Gradient.GetNormalized();
-				Image->SetPixel(x, y, color4i(Clamp<s32>(GradNormal.X * 128 + 128, 0, 255), Clamp<s32>(GradNormal.Y * 128 + 128, 0, 255), 0, 0));
-			}
-			Image->Write(s1.str());
-				std::stringstream s2;
-				s2 << "OutputPostNormalsMagnitude" << t << ".bmp";
-			MaxMagnitude = 0;
-			for (u32 y = 0; y < Height; ++ y)
-			for (u32 x = 0; x < Width; ++ x)
-				MaxMagnitude = Maximum(MaxMagnitude, GetPoint(x, y).Gradient.Length());
-			printf("Maximum post normal mag is %f\n", MaxMagnitude);
-			for (u32 y = 0; y < Height; ++ y)
-			for (u32 x = 0; x < Width; ++ x)
-			{
-				Image->SetPixel(x, y, color4f(GetPoint(x, y).Gradient.Length() / MaxMagnitude));
-			}
-			Image->Write(s2.str());
-
 		}
 		P.End();
-		fclose(f);
+		//fclose(f);
+		
+		// Diagnostic
+		for (u32 y = 0; y < Height; ++ y)
+		for (u32 x = 0; x < Width; ++ x)
+		{
+			vec2f GradNormal = GetPoint(x, y).Gradient.GetNormalized();
+			Image->SetPixel(x, y, color4i(Clamp<s32>(GradNormal.X * 128 + 128, 0, 255), Clamp<s32>(GradNormal.Y * 128 + 128, 0, 255), 0, 0));
+		}
+		Image->Write("OutputPostNormals.bmp");
+		MaxMagnitude = 0;
+		for (u32 y = 0; y < Height; ++ y)
+		for (u32 x = 0; x < Width; ++ x)
+			MaxMagnitude = Maximum(MaxMagnitude, GetPoint(x, y).Gradient.Length());
+		//printf("Maximum post normal mag is %f\n", MaxMagnitude);
+		for (u32 y = 0; y < Height; ++ y)
+		for (u32 x = 0; x < Width; ++ x)
+		{
+			Image->SetPixel(x, y, color4f(GetPoint(x, y).Gradient.Length() / MaxMagnitude));
+		}
+		Image->Write("OutputPostNormalsMagnitude.bmp");
 
 
 		// Blur gradient Pass
 		printf("Gradient blur... ");
 		P.Begin();
-		static s32 const GradientSigma = 6;
+		static s32 const GradientSigma = 30;
 		static s32 const GradientSpread = 3;
-		for (u32 y = 0; y < Height; ++ y)
-		for (u32 x = 0; x < Width; ++ x)
+		static u32 const GradientBlurPasses = 3;
+		for (u32 t = 0; t < GradientBlurPasses; ++ t)
 		{
-			GetPoint(x, y).PingPongGradient = 0;
-			for (s32 i = 1; i < GradientSigma * GradientSpread; ++ i)
-				GetPoint(x, y).PingPongGradient += Gaussian<f32>(i, GradientSigma) * (GetPoint(x+i, y).Gradient + GetPoint(x-i, y).Gradient);
-			GetPoint(x, y).PingPongGradient += Gaussian<f32>(0, GradientSigma) * GetPoint(x, y).Gradient;
-			P.Update(50 * y / Height);
-		}
-		for (u32 y = 0; y < Height; ++ y)
-		for (u32 x = 0; x < Width; ++ x)
-		{
-			GetPoint(x, y).Gradient = GetPoint(x, y).PingPongGradient;
-		}
-		for (u32 y = 0; y < Height; ++ y)
-		for (u32 x = 0; x < Width; ++ x)
-		{
-			GetPoint(x, y).PingPongGradient = 0;
-			for (s32 i = 1; i < GradientSigma * GradientSpread; ++ i)
-				GetPoint(x, y).PingPongGradient += Gaussian<f32>(i, GradientSigma) * (GetPoint(x, y+i).Gradient + GetPoint(x, y-i).Gradient);
-			GetPoint(x, y).PingPongGradient += Gaussian<f32>(0, GradientSigma) * GetPoint(x, y).Gradient;
-			P.Update(50 + 50 * y / Height);
-		}
-		for (u32 y = 0; y < Height; ++ y)
-		for (u32 x = 0; x < Width; ++ x)
-		{
-			GetPoint(x, y).Gradient = GetPoint(x, y).PingPongGradient;
+			for (u32 y = 0; y < Height; ++ y)
+			for (u32 x = 0; x < Width; ++ x)
+			{
+				GetPoint(x, y).PingPongGradient = 0;
+				for (s32 i = 1; i < GradientSigma * GradientSpread; ++ i)
+					GetPoint(x, y).PingPongGradient += Gaussian<f32>(i, GradientSigma) * (GetPoint(x+i, y).Gradient + GetPoint(x-i, y).Gradient);
+				GetPoint(x, y).PingPongGradient += Gaussian<f32>(0, GradientSigma) * GetPoint(x, y).Gradient;
+				P.Update(100 * y / Height / 2 / GradientBlurPasses + 100 * t / GradientBlurPasses);
+			}
+			for (u32 y = 0; y < Height; ++ y)
+			for (u32 x = 0; x < Width; ++ x)
+			{
+				GetPoint(x, y).Gradient = GetPoint(x, y).PingPongGradient;
+			}
+			for (u32 y = 0; y < Height; ++ y)
+			for (u32 x = 0; x < Width; ++ x)
+			{
+				GetPoint(x, y).PingPongGradient = 0;
+				for (s32 i = 1; i < GradientSigma * GradientSpread; ++ i)
+					GetPoint(x, y).PingPongGradient += Gaussian<f32>(i, GradientSigma) * (GetPoint(x, y+i).Gradient + GetPoint(x, y-i).Gradient);
+				GetPoint(x, y).PingPongGradient += Gaussian<f32>(0, GradientSigma) * GetPoint(x, y).Gradient;
+				P.Update(100 * (y + Height) / Height / 2 / GradientBlurPasses + 100 * t / GradientBlurPasses);
+			}
+			for (u32 y = 0; y < Height; ++ y)
+			for (u32 x = 0; x < Width; ++ x)
+			{
+				GetPoint(x, y).Gradient = GetPoint(x, y).PingPongGradient;
+			}
 		}
 		P.End();
 		
@@ -297,17 +296,20 @@ public:
 				GetPoint(x, y).Height = 0;
 
 				f32 Accumulator = 0;
-				s32 const SquareSize = 4;
+				s32 const SquareSize = 5;
 				for (s32 i = - SquareSize / 2; i < SquareSize / 2; ++ i)
 				for (s32 j = - SquareSize / 2; j < SquareSize / 2; ++ j)
 				{
-					if (GetPoint(x+i, y+j).Set)
+					if (i != 0 || j != 0)
 					{
 						vec2f const Offset(vec2i(i, j));
-						f32 const Weight = (1 / Offset.Length()) * Offset.Dot(GetPoint(x+i, y+j).Gradient);
+						if (Offset.LengthSq() <= Sq(SquareSize) && GetPoint(x+i, y+j).Set)
+						{
+							f32 const Weight = (1 / Offset.Length()) * Offset.GetNormalized().Dot(GetPoint(x+i, y+j).Gradient.GetNormalized());
 
-						GetPoint(x, y).Height += Weight * (GetPoint(x+i, y+j).Height + Offset.Length() * Offset.Dot(GetPoint(x+i, y+j).Gradient));
-						Accumulator += Weight;
+							GetPoint(x, y).Height += Weight * (GetPoint(x+i, y+j).Height + Offset.Length() * Offset.Dot(GetPoint(x+i, y+j).Gradient));
+							Accumulator += Weight;
+						}
 					}
 				}
 
@@ -323,14 +325,20 @@ public:
 		P.End();
 
 		
+		f32 MinHeight = 0, MaxHeight = 0;
 		for (u32 y = 0; y < Height; ++ y)
 		for (u32 x = 0; x < Width; ++ x)
 		{
-			u8 ColorValue = Clamp<s32>(255 - GetPoint(x, y).Height, 0, 255);
-			Image->SetPixel(x, y, color4i(ColorValue));
+			MinHeight = Minimum(MinHeight, GetPoint(x, y).Height);
+			MaxHeight = Maximum(MaxHeight, GetPoint(x, y).Height);
+		}
+		for (u32 y = 0; y < Height; ++ y)
+		for (u32 x = 0; x < Width; ++ x)
+		{
+			Image->SetPixel(x, y, color4f((GetPoint(x, y).Height - MinHeight)/(MaxHeight - MinHeight)));
 		}
 
 		Image->Write("Output.bmp");
-		WaitForUser();
+		//WaitForUser();
 	}
 };
